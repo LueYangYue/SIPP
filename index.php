31d39797e0ab64a15a26df68d2e75e8146de6496
LueYangYue <a202211@siswa.ukm.edu.my>
2026-07-22 10:57:17 +0800

    update content

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
 <html>
-<head><link rel="icon" type="image/x-icon" href="https://ukmsipp.me/SIPP/images/university.png"></head>
+<head>
+  <link rel="icon" type="image/x-icon" href="https://ukmsipp.me/SIPP/images/university.png">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <body>
-<h1>Selamat datang ke <b>S</b>istem <b>I</b>ntervensi <b>P</b>restasi <b>P</b>elajar</h1>
-<p>Sila gunakan <a href="https://sipp-app-uaf9v.ondigitalocean.app">aplikasi SIPP</a><br /><b>ATAU</b>
-<br />Pergi ke <a href="https://lueyangyue.github.io/SIPP/landing.html">halaman pendaratan.</a>
+<div class="container">
+    <span class="glyphicon glyphicon-map-marker"></span>
+    <h1>Welcome to My Student Performance Intervention System</h1>
+    <h2>Selamat Datang ke SIPP: Sistem Intervensi Prestasi Pelajar</h2>
+    <br /><br />Link to SIPP <a href="https://lueyangyue.github.io/SIPP/landing.html">landing page</a>
+    <br />Pautan <a href="https://lueyangyue.github.io/SIPP/landing.html">halaman pendaratan</a>
+</div>
 </body>
 </html>
