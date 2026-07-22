69f5b83a6713dba01efc1f90c6061de1de161733
LueYangYue <a202211@siswa.ukm.edu.my>
2026-07-22 12:34:02 +0800

    change link

--- index.php
+++ index.php
@@ -9,16 +9,16 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
     span {
-      font-size: xx-large;
+      font-size: medium;
     }
   </style>
 </head>
 <body>
 <div class="container">
-    <h1>Welcome to My <b>Student Performance Intervention System</b></h1>
-    <h2>Selamat Datang ke <b>SIPP: Sistem Intervensi Prestasi Pelajar</b></h2><span class="glyphicon glyphicon-map-marker"></span>
-    <br /><br />Link to <strong>SIPP</strong> <a href="https://lueyangyue.github.io/SIPP/landing.html">landing page</a>
-    <br />Pautan <a href="https://lueyangyue.github.io/SIPP/landing.html">halaman pendaratan</a>
+  <h1>Welcome to My <b>Student Performance Intervention System</b></h1>
+  <h2>Selamat Datang ke <b>SIPP: Sistem Intervensi Prestasi Pelajar</b></h2>
+  <br /><br />Link to <strong>SIPP</strong> <a href="landing.html">landing page</a>
+  <br />Pautan <a href="landing.html">halaman pendaratan</a></h2><span class="glyphicon glyphicon-map-marker"></span>
 </div>
 </body>
 </html>
