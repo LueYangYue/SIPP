0ad9b70fcc802c11743440d32cbcb2901ee9b3c4
LueYangYue <a202211@siswa.ukm.edu.my>
2026-07-22 11:51:59 +0800

    formatting

--- index.php
+++ index.php
@@ -7,13 +7,17 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    span {
+      font-size: xx-large;
+    }
+  </style>
 </head>
 <body>
 <div class="container">
-    <span class="glyphicon glyphicon-map-marker"></span>
-    <h1>Welcome to My Student Performance Intervention System</h1>
-    <h2>Selamat Datang ke SIPP: Sistem Intervensi Prestasi Pelajar</h2>
-    <br /><br />Link to SIPP <a href="https://lueyangyue.github.io/SIPP/landing.html">landing page</a>
+    <h1>Welcome to My <b>Student Performance Intervention System</b></h1>
+    <h2>Selamat Datang ke <b>SIPP: Sistem Intervensi Prestasi Pelajar</b></h2><span class="glyphicon glyphicon-map-marker"></span>
+    <br /><br />Link to <strong>SIPP</strong> <a href="https://lueyangyue.github.io/SIPP/landing.html">landing page</a>
     <br />Pautan <a href="https://lueyangyue.github.io/SIPP/landing.html">halaman pendaratan</a>
 </div>
 </body>
